ee23b98c80ffcfc697cd4fbecc8fc1f295caa538
Ori Pridan <oripridan@gmail.com>
2025-11-23 20:26:41 +0000

    chore(release): v2.1.37 [skip ci]

--- src/precog/index.ts
+++ src/precog/index.ts
@@ -1,13 +1,16 @@
-// @version 2.1.28
+// @version 2.1.37
 import { bus } from "../core/event-bus.js";
 import { TrainingService } from "./training/index.js";
+import { ProviderEngine } from "./provider-engine.js";
 
 export class Precog {
   public training: TrainingService;
+  public providers: ProviderEngine;
 
   constructor() {
     console.log("[Precog] Initializing Predictive Intelligence...");
     this.training = new TrainingService(process.cwd());
+    this.providers = new ProviderEngine();
   }
 
   public async init() {
