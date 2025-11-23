76720030ea75902d2351616bf1f3dd612553738a
Ori Pridan <oripridan@gmail.com>
2025-11-23 23:58:33 +0000

    chore(release): v2.1.97 [skip ci]

--- projects/test/bad.ts
+++ projects/test/bad.ts
@@ -1,4 +1,2 @@
-// @version 2.1.96
-const x: number = 42; // This will be a valid TypeScript code
-// To create a type error, you can use:
-// const y: number = "string";
+// @version 2.1.97
+let x: number = 42;
