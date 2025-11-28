e236e147d7f4059ed1096127c720d46214f3d957
Ori Pridan <oripridan@gmail.com>
2025-11-28 08:34:04 +0000

    chore(release): v2.2.59 [skip ci]

--- src/web-app/src/components/Layout.jsx
+++ src/web-app/src/components/Layout.jsx
@@ -1,15 +1,20 @@
-// @version 2.2.26
+// @version 2.2.59
 import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import MainContent from './MainContent';
 
-const Layout = ({ children, setActiveComponent }) => {
+const Layout = ({ children, setActiveComponent, activeComponent, currentSessionId, setCurrentSessionId }) => {
   return (
     <div className="flex flex-col h-screen bg-obsidian text-gray-300">
       <Header />
       <div className="flex flex-1 overflow-hidden">
-        <Sidebar setActiveComponent={setActiveComponent} />
+        <Sidebar 
+          setActiveComponent={setActiveComponent} 
+          activeComponent={activeComponent}
+          currentSessionId={currentSessionId}
+          setCurrentSessionId={setCurrentSessionId}
+        />
         <MainContent>
           {children}
         </MainContent>
