c22d1c976ad8d17d5e4087e42028063a9365e75d
Ori Pridan <oripridan@gmail.com>
2025-12-12 09:25:26 +0000

    chore(release): v3.3.534 [skip ci]

---
+++ src/web-app/src/skin/components/index.js
@@ -0,0 +1,22 @@
+// @version 3.3.534
+/**
+ * @file Components Index - Skin Component Exports
+ * @version 3.3.531
+ * 
+ * Central export for all Liquid Synapsys UI components.
+ */
+
+// Core Layout
+export { default as ModeLayout, ModeSwitcher } from './ModeLayout.jsx';
+export { default as ControlDeck } from './ControlDeck.jsx';
+export { default as KnowledgeRail } from './KnowledgeRail.jsx';
+export { default as SegmentationSidebar } from './SegmentationSidebar.jsx';
+
+// Input & Interaction
+export { default as UniversalInput } from './UniversalInput.jsx';
+export { default as ProjectSelector } from './ProjectSelector.jsx';
+export { CommandPalette, useCommandPalette } from './CommandPalette.jsx';
+
+// Visualization
+export { default as TaskDAG } from './TaskDAG.jsx';
+export { default as SystemPulse } from './SystemPulse.jsx';
